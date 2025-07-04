225aa18e5155b92fdd01cb8bf3b5ca0dd458b5f4
Anthony Tran-Phan <tonyphan15@gmail.com>
2025-07-04 15:48:04 -0700

    Create transaction history modal component

---
+++ money_ui/src/app/Components/dashboard-page/transaction-history-modal/transaction-history-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-history-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-history-modal.component.html',
+  styleUrl: './transaction-history-modal.component.css'
+})
+export class TransactionHistoryModalComponent {
+
+}
