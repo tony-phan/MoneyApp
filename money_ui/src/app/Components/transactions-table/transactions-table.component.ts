1727e24c1c14c945b3412320ef1b89d9e64c0d69
Anthony Tran-Phan <tonyphan15@gmail.com>
2025-07-09 23:49:02 -0700

    add pagination to transactions table component

--- money_ui/src/app/Components/transactions-table/transactions-table.component.ts
+++ money_ui/src/app/Components/transactions-table/transactions-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, signal, ViewChild } from '@angular/core';
 import { Transaction } from '../../_models/transaction';
 import { MaterialModule } from '../../material.module';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-transactions-table',
@@ -13,9 +14,41 @@ export class TransactionsTableComponent {
   @Input() transactions: Transaction[] = [];
   @Output() rowClicked = new EventEmitter<Transaction>();
 
+  pagination = signal({ pageIndex: 0, pageSize: 8 });
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  totalPages = computed(() => {
+    Math.ceil(this.transactions.length / this.pagination().pageSize)
+  });
+  paginatedTransactions = computed(() => {
+    const {pageIndex, pageSize} = this.pagination();
+    const start = pageIndex * pageSize;
+    return this.transactions.slice(start, start + pageSize);
+  });
+
   columns: string[] = ['date', 'amount', 'type', 'category', 'description'];
+
+  constructor() {
+    this.pagination.set({
+      pageIndex: 0,
+      pageSize: this.pagination().pageSize
+    });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pagination.set({
+      pageIndex: event.pageIndex,
+      pageSize: event.pageSize
+    });
+  }
 
   getCategory(transaction: Transaction): string {
     return transaction.transactionType === "Income" ? transaction.incomeCategory ?? '' : transaction.expenseCategory ?? '';
+  }
+
+  // format date to MM/DD/YYYY
+  formatDate(date: Date): string {
+    return `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}/${date.getFullYear()}`;
   }
 }
