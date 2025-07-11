323c15840609bc3ac2869bb43033b617cdb2d275
Anthony Tran-Phan <tonyphan15@gmail.com>
2025-07-10 20:32:40 -0700

    worked on styling for create transaction history modal

--- money_ui/src/app/Components/dashboard-page/create-transaction-history-modal/create-transaction-history-modal.component.ts
+++ money_ui/src/app/Components/dashboard-page/create-transaction-history-modal/create-transaction-history-modal.component.ts
@@ -3,29 +3,32 @@ import { MaterialModule } from '../../../material.module';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSelect } from "@angular/material/select";
+import { MONTHS } from '../../../constants';
 
 @Component({
   selector: 'app-create-transaction-history-modal',
   standalone: true,
-  imports: [MaterialModule, CommonModule, ReactiveFormsModule],
+  imports: [MaterialModule, CommonModule, ReactiveFormsModule, MatSelect],
   templateUrl: './create-transaction-history-modal.component.html',
   styleUrl: './create-transaction-history-modal.component.css'
 })
 export class CreateTransactionHistoryModalComponent {
+  monthOptions = MONTHS;
   form: FormGroup;
-
+  
   constructor(
     private fb: FormBuilder, 
     private dialogRef: MatDialogRef<CreateTransactionHistoryModalComponent>
   ) {
     this.form = this.fb.group({
-      month: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+      month: ['', [Validators.required]],
       year: ['', [Validators.required, Validators.min(1900), Validators.max(2100)]],
     });
   }
 
   submit(): void {
-    if(this.form.valid) 
+    if(this.form.valid)
       this.dialogRef.close(this.form.value);
   }
 
